5af23a4609b5b83af70a48b0f3e83e3e36b95b66
brianpchsu <brianpchsu@gmail.com>
2015-02-14 11:43:05 -0800

    Finished basic requirement.

--- watchout.js
+++ watchout.js
@@ -1,4 +1,11 @@
-// start slingin' some d3 here.
+// start slingin" some d3 here.
+var radius = 10;
+var width = 800;
+var height = 800;
+var isCollided = false;
+var currentScore = parseInt(d3.select("#currentScore").text());
+var highScore = parseInt(d3.select("#highScore").text());
+
 var Enemy = function() {
   this.top = randomGenerator();
   this.left = randomGenerator();
@@ -28,28 +35,25 @@ var drag = d3.behavior.drag()
             .on("dragend", dragend);
 
 function init(input, color) {
-  // console.log("hi");
+  //Enemies
   var circle = svg.selectAll("circle")
             .data(input)
             .enter()
             .append("circle")
-            .attr('class', 'enemy')
-            .attr("cx", function(d) { return d.top })
-            .attr("cy", function(d) { return d.left })
-            .attr("r", function(d) { return d.radius })
-            .style('fill', color);
-
-  svg.append('circle')
+            .attr("class", "enemy")
+            .attr("cx", function(d) { return d.top;})
+            .attr("cy", function(d) { return d.left; })
+            .attr("r", function(d) { return d.radius; })
+            .style("fill", color);
+  //Player
+  svg.append("circle")
     .attr("cx", function(d) { return 400; })
     .attr("cy", function(d) { return 400 })
     .attr("r", function(d) { return 10 })
-    .attr('class', 'player')
-    .style('fill', 'green')
+    .attr("class", "player")
+    .style("fill", "green")
     .call(drag);
 }
-var radius = 10;
-var width = 800;
-var height = 800;
 
 function dragstart(d){
   d3.select(this)
@@ -71,36 +75,42 @@ function update(){
     .duration(1000)
     .attr("cx", randomGenerator)
     .attr("cy", randomGenerator);
+
   // if isCollided
+  if (isCollided){
+    var numCollided = parseInt(d3.select("#numCollisions").text());
+    numCollided += 1;
+    d3.select("#numCollisions").text(numCollided);
+    currentScore = 0;
+    d3.select("#currentScore").text(currentScore);
+    isCollided = false;
+  }else {
+    currentScore++;
+    d3.select("#currentScore").text(currentScore);
+    if (currentScore >highScore){
+      highScore = currentScore;
+      d3.select("#highScore").text(highScore);
+    }
+  }
 }
-
-
 
 var checkCollision = function() {
-  // console.log(player)
-
   d3.selectAll(".enemy").each(function(d){
-    var playerX = d3.select('.player').attr('cx');
-    var playerY = d3.select('.player').attr('cy');
+    var playerX = d3.select(".player").attr("cx");
+    var playerY = d3.select(".player").attr("cy");
     var currentEnemy = d3.select(this);
-    // console.log(this.attr('cy'));
-    var dis = Math.sqrt(Math.pow(currentEnemy.attr('cx') - playerX,2) + Math.pow(currentEnemy.attr('cy') - playerY,2));
+    var dis = Math.sqrt(Math.pow(currentEnemy.attr("cx") - playerX,2) + Math.pow(currentEnemy.attr("cy") - playerY,2));
 
     if (dis < 10) {
-      console.log(dis);
-      var isCollided = parseInt(d3.select('#numCollisions').text());
-      isCollided += 1;
-      d3.select('#numCollisions').text(isCollided);
+      isCollided = true;
     }
   });
-}
+};
 
 init(createEnemies(10), "red");
-// init(new Player(), 'green');
 
 setInterval(function(){
   update();
 }, 1000);
-
 setInterval(checkCollision, 20);
 
