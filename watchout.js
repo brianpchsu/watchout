e80065e3ee714660a6a96f8858c1acef819caa80
brianpchsu <brianpchsu@gmail.com>
2015-02-13 19:45:41 -0800

    Finish all enemies and player setup.

--- watchout.js
+++ watchout.js
@@ -5,12 +5,6 @@ var Enemy = function() {
   this.radius = 10;
 };
 
-var Player = function() {
-  this.top = 400;
-  this.left = 400;
-  this.radius = 10;
-};
-
 var createEnemies = function(n){
   var enemies = [];
   for( var i =0; i < n; i++){
@@ -28,20 +22,51 @@ var svg = d3.select("body").append("svg")
    .attr("width", 800)
    .attr("height", 800);
 
+var drag = d3.behavior.drag()
+            .on("dragstart", dragstart)
+            .on("drag", dragmove)
+            .on("dragend", dragend);
+
 function init(input, color) {
   // console.log("hi");
   var circle = svg.selectAll("circle")
             .data(input)
             .enter()
             .append("circle")
+            .attr('class', 'enemy')
             .attr("cx", function(d) { return d.top })
             .attr("cy", function(d) { return d.left })
             .attr("r", function(d) { return d.radius })
             .style('fill', color);
+
+  svg.append('circle')
+    .attr("cx", function(d) { return 400; })
+    .attr("cy", function(d) { return 400 })
+    .attr("r", function(d) { return 10 })
+    .attr('class', 'player')
+    .style('fill', 'green')
+    .call(drag);
+}
+var radius = 10;
+var width = 800;
+var height = 800;
+
+function dragstart(d){
+  d3.select(this)
+    .style("fill", "gold")
+}
+function dragmove(d) {
+  d3.select(this)
+      .attr("cx", d3.event.x)
+      .attr("cy", d3.event.y);
+}
+function dragend(d){
+  d3.select(this)
+    .style("fill", "green")
 }
 
 function update(){
-  svg.selectAll("circle")
+  svg.selectAll("circle.enemy")
     .transition()
     .duration(1000)
     .attr("cx", randomGenerator)
@@ -49,27 +74,10 @@ function update(){
 }
 
 init(createEnemies(10), "red");
-init(new Player(), 'green');
+// init(new Player(), 'green');
 
 setInterval(function(){
   update();
-}, 1000);
+}, 2000);
 
-var Player = function() {
-  this.top = 400;
-  this.left = 400;
-  this.radius = 10;
-};
-function addPlayer (input){
-  var rect = svg.selectAll("rect")
-            .data(input)
-            .enter()
-            .append("rect")
-            .attr("rx", function(d) { return d.top })
-            .attr("ry", function(d) { return d.left })
-            .attr("width", 10)
-            .attr("height", 10)
-            .style('fill', 'green');
-}
-var player = new Player();
-addPlayer(player);
+
